7b2fd9403d24bab3edb799a8c67762ccbd91e8bc
Louis Hough <sirlouishough@gmail.com>
2020-01-22 22:18:17 +0000

    calculator memory (array), add button working in running total

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
 //global variables
+let calcMemory = [];
 const numButtons = document.getElementsByClassName("numBtn");
+const addButton = document.getElementById("add");
 let runningInput = document.getElementById("runninginput");
+
+addButton.addEventListener("click", userAdd, false);
 
 //populate display function
 const populateDisplay = function(){
     const value = this.getAttribute("data-value");
+    calcMemory.push(parseInt(value));
     runningInput.textContent += value;
 }
 for (var i = 0; i < numButtons.length; i++){
     numButtons[i].addEventListener('click', populateDisplay, false);
+}
+
+//addbutton
+function userAdd() {
+    if(calcMemory.length > 2) {
+        runningInput.textContent = operate(add, calcMemory[0], calcMemory[2]);
+        calcMemory = [operate(add, calcMemory[0], calcMemory[2])];
+    }
+    calcMemory.push("+")
+    runningInput.textContent += "+";
 }
 
 //operation selection function
