eb74f2b8c905c478069c5d5acc72b02851f929db
Louis Hough <sirlouishough@gmail.com>
2020-01-22 19:18:14 +0000

    operation selection function

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
-//basic operations
-add = (a,b) => a + b;
-subtract = (a,b) => a - b;
-multiply = (a,b) => a * b;
-divide = (a,b) => a / b;
+//operation selection function
+operate = (operation, n1, n2) => {
+    switch(operation){
+        case add:
+        result = add(n1,n2);
+        break;
+        case subtract:
+        result = subtract(n1, n2);
+        break;
+        case multiply:
+        result = multiply(n1, n2);
+        break;
+        case divide:
+        result = divide(n1, n2);
+        break;
+    }
+    return result;
+}
+
+//basic operation functions
+add = (n1,n2) =>  n1 + n2;
+subtract = (n1,n2) => n1 - n2;
+multiply = (n1,n2) => n1 * n2;
+divide = (n1,n2) => n1 / n2;
