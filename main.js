c9acb1528798590a5f3300ee1a577ca6ce75c1ec
Louis Hough <sirlouishough@gmail.com>
2020-01-22 21:26:56 +0000

    user input function for numerical buttons with event listener to populate running input display

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+//global variables
+const numButtons = document.getElementsByClassName("numBtn");
+let runningInput = document.getElementById("runninginput");
+
+//populate display function
+const populateDisplay = function(){
+    const value = this.getAttribute("data-value");
+    runningInput.textContent += value;
+}
+for (var i = 0; i < numButtons.length; i++){
+    numButtons[i].addEventListener('click', populateDisplay, false);
+}
+
 //operation selection function
 operate = (operation, n1, n2) => {
     switch(operation){
