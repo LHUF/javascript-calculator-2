924973ed6088cd260f2c42ec40b0092b05262f81
Louis Hough <sirlouishough@gmail.com>
2020-01-30 00:54:10 +0000

    formatting

--- main.js
+++ main.js
@@ -9,98 +9,97 @@ const equalsButton = document.getElementById("equals");
 const resultDisp = document.getElementById("result");
 let answer;
 
-const populateDisplay = function(){
-    const value = this.getAttribute("data-value");
-    runningInput.textContent += value;
-}
-for (var i = 0; i < numButtons.length; i++){
-    numButtons[i].addEventListener('click', populateDisplay, false);
+const populateDisplay = function() {
+  const value = this.getAttribute("data-value");
+  runningInput.textContent += value;
+};
+for (var i = 0; i < numButtons.length; i++) {
+  numButtons[i].addEventListener("click", populateDisplay, false);
 }
 
-operatorBtns.forEach(function(btn){
-    btn.addEventListener("click", operatorHandler);
-  });
+operatorBtns.forEach(function(btn) {
+  btn.addEventListener("click", operatorHandler);
+});
 
-function collapseMemStack(calcMemory){
-    const workingStack = calcMemory;
-    while(workingStack.length > 1){
+function collapseMemStack(calcMemory) {
+  const workingStack = calcMemory;
+  while (workingStack.length > 1) {
     firstNum = parseInt(calcMemory.shift());
-    op = calcMemory.shift()
+    op = calcMemory.shift();
     secondNum = parseInt(calcMemory.shift());
     let answer = operate(op, firstNum, secondNum);
     workingStack.unshift(answer);
-    }
-    return workingStack;
+  }
+  return workingStack;
 }
 
-function operatorHandler(eventObj){
-    const clickedEl = eventObj.target;
-    const operator = clickedEl.textContent;
-    const split = runningInput.textContent.split(/[×÷+-]/);
-    calcMemory.push((split[split.length -1]));
-    calcMemory = collapseMemStack(calcMemory);
-    calcMemory.push(operator);
-    if(answer === undefined){
-        runningInput.textContent += operator;
-        return;
-    } else runningInput.textContent =+ answer + operator;
-  }
+function operatorHandler(eventObj) {
+  const clickedEl = eventObj.target;
+  const operator = clickedEl.textContent;
+  const split = runningInput.textContent.split(/[×÷+-]/);
+  calcMemory.push(split[split.length - 1]);
+  calcMemory = collapseMemStack(calcMemory);
+  calcMemory.push(operator);
+  if (answer === undefined) {
+    runningInput.textContent += operator;
+    return;
+  } else runningInput.textContent = +answer + operator;
+}
 
 equalsButton.addEventListener("click", equals, false);
-function equals(){
-    const lastInput = Number(runningInput.textContent[runningInput.textContent.length-1]);
-    if (isNaN(lastInput)){
-        runningInput.textContent = runningInput.textContent.slice(0,-1);
-        resultDisp.textContent = runningInput.textContent;
-    }
-    else {  
-        calcMemory.push(lastInput);
-        resultDisp.textContent = collapseMemStack(calcMemory);
-    }
-
+function equals() {
+  const lastInput = Number(
+    runningInput.textContent[runningInput.textContent.length - 1]
+  );
+  if (isNaN(lastInput)) {
+    runningInput.textContent = runningInput.textContent.slice(0, -1);
+    resultDisp.textContent = runningInput.textContent;
+  } else {
+    calcMemory.push(lastInput);
+    resultDisp.textContent = collapseMemStack(calcMemory);
+  }
 }
 
 operate = (operation, n1, n2) => {
-    switch(operation){
-        case "+":
-        answer = add(n1,n2);
-        break;
-        case "-":
-        answer = subtract(n1, n2);
-        break;
-        case "×":
-        answer = multiply(n1, n2);
-        break;
-        case "÷":
-        answer = divide(n1, n2);
-        break;
-    }
-    return answer;
+  switch (operation) {
+    case "+":
+      answer = add(n1, n2);
+      break;
+    case "-":
+      answer = subtract(n1, n2);
+      break;
+    case "×":
+      answer = multiply(n1, n2);
+      break;
+    case "÷":
+      answer = divide(n1, n2);
+      break;
+  }
+  return answer;
+};
+
+acButton.addEventListener("click", allCancel, false);
+function allCancel() {
+  runningInput.textContent = "";
+  calcMemory = [];
+  answer = undefined;
+  resultDisp.textContent = "";
 }
 
- acButton.addEventListener("click", allCancel, false);
- function allCancel(){
- runningInput.textContent = "";
- calcMemory = [];
- answer = undefined;
- resultDisp.textContent = "";
- }
+delButton.addEventListener("click", delLastChar, false);
+function delLastChar() {
+  let lastItem = calcMemory.pop();
+  if (lastItem === "+" || "-" || "×" || "÷") {
+    runningInput.textContent = runningInput.textContent.slice(0, -1);
+    calcMemory.pop();
+    calcMemory.pop();
+  } else {
+    calcMemory.push(lastItem);
+    runningInput.textContent = runningInput.textContent.slice(0, -1);
+  }
+}
 
- delButton.addEventListener("click", delLastChar, false);
- function delLastChar(){
-     let lastItem = calcMemory.pop()
-     if(lastItem === "+" || "-" || "×" || "÷") {
-        runningInput.textContent = runningInput.textContent.slice(0,-1);
-        calcMemory.pop();
-        calcMemory.pop();    
-     }
-     else {
-     calcMemory.push(lastItem);
-     runningInput.textContent = runningInput.textContent.slice(0,-1);
-    }
- }
-
-add = (n1,n2) =>  n1 + n2;
-subtract = (n1,n2) => n1 - n2;
-multiply = (n1,n2) => n1 * n2;
-divide = (n1,n2) => n1 / n2;
+add = (n1, n2) => n1 + n2;
+subtract = (n1, n2) => n1 - n2;
+multiply = (n1, n2) => n1 * n2;
+divide = (n1, n2) => n1 / n2;
